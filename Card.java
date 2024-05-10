682d6b73623d0594a2ccc98c7186fc1598de5a7d
unknown <23054220@siswa.um.edu.my>
2024-05-10 22:11:11 +0800

    card and transaction class

---
+++ Card.java
@@ -0,0 +1,103 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+import java.util.Date;
+
+
+public class Card {
+    private String cardNum;
+    private int cVV;
+    private Date expiryDate;
+    private String userId;
+    private static List<String> generatedCardNums = new ArrayList<>();
+    Random random = new Random();
+
+
+    public Card(String userId) {
+        this.userId = userId;
+        generateCardNum();
+        generateCVV();
+        generateExpiryDate();
+    }
+
+    public String getCardNum() {
+
+        return cardNum;
+    }
+
+    public void generateCardNum() {
+//        generate a 16-digit card number
+        StringBuilder stringBuilder = new StringBuilder();
+        do {
+            for (int i = 0; i < 16; i++) {
+                stringBuilder.append(random.nextInt(10));
+            }
+        } while (generatedCardNums.contains(stringBuilder.toString()));
+//        avoid duplicate card numbers
+        this.cardNum = stringBuilder.toString();
+        generatedCardNums.add(cardNum);
+    }
+
+    public int getCVV() {
+
+        return cVV;
+    }
+
+    public void generateCVV() {
+//generate a 3-digit cvv
+        this.cVV = random.nextInt(900) + 100;
+    }
+
+    public String getExpiryDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM / YY");
+
+        return dateFormat.format(expiryDate);
+    }
+
+    public void generateExpiryDate() {
+        Calendar calendar = Calendar.getInstance();
+//        the card will expire in 10 years
+
+        calendar.add(Calendar.YEAR, 10);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        this.expiryDate = calendar.getTime();
+    }
+
+    public boolean isExpired() {
+        return expiryDate.before(new Date());
+    }
+
+    public void updateExpiryDate() {
+
+        Calendar currentCalendar = Calendar.getInstance();
+        Calendar expiryCalendar = Calendar.getInstance();
+//            user can update the expiration time within three months before the expiration date
+        currentCalendar.add(Calendar.MONTH, 3);
+        if (currentCalendar.after(expiryDate)) {
+            expiryCalendar.setTime(expiryDate);
+//            Update expiry date by 20 years
+            expiryCalendar.add(Calendar.YEAR, 5);
+            expiryDate = expiryCalendar.getTime();
+        } else {
+            System.out.println("Your card will expire for a long time so don't need to update it yet.");
+        }
+
+    }
+
+    public String getUserId() {
+
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" + "cardNum='" + cardNum + '\'' + ", cVV=" + cVV + ", expiryDate=" + expiryDate + ", userId='" + userId + '\'' + '}';
+    }
+}
---
+++ Transaction.java
@@ -0,0 +1,59 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Transaction {
+    private String userId;
+    private double amount;
+    private double balance;
+    private Date dateOfTrans;
+    private String category;
+
+    public Transaction(String userId, double amount, double balance, String category) {
+        this.userId = userId;
+        this.amount = amount;
+        this.balance = balance;
+        this.dateOfTrans = new Date();
+        this.category = category;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+
+    public double getAmount() {
+        return amount;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public Date getDateOfTrans() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yy");
+        return dateOfTrans;
+    }
+
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isValid() {
+        return amount > 0 && amount <= balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "userId='" + userId + '\'' +
+                ", amount=" + amount +
+                ", balance=" + balance +
+                ", dateOfTrans=" + dateOfTrans +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
+
