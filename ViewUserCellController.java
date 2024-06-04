c8c73bdfc848b2c75929536e6748af377d119048
phoebe2z <149281825+phoebe2z@users.noreply.github.com>
2024-06-04 15:29:04 +0800

    Update ViewUserCellController.java
    Signed-off-by: phoebe2z <149281825+phoebe2z@users.noreply.github.com>

--- ViewUserCellController.java
+++ ViewUserCellController.java
@@ -17,9 +17,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
+import Database.Constant;
 
-public class ViewUserCellController implements Initializable {
+public class ViewUserCellController extends ListCell<UserAvatar> implements Initializable {
 
 
     @FXML
@@ -51,7 +53,12 @@ public class ViewUserCellController implements Initializable {
 
     @FXML
     private Button ViewBtn;
-    
+    private Account user;
+    private ViewUserController parentController;
+    private FXMLLoader loader;
+    private Map<Integer, String> userInfoName = new HashMap<>();
+    private Map<Integer, Integer> userInfoNum = new HashMap<>();
+
     
 
     @FXML
@@ -63,13 +70,92 @@ public class ViewUserCellController implements Initializable {
     void View_Btn(ActionEvent event) {
 
     }
-    
+
+    public ViewUserCellController(ViewUserController parentController) {
+        this.parentController = parentController;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXMLFiles/ViewUserCell.fxml"));
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        loadAccountsFromDatabase();
+        ViewBtn.setOnAction(event -> {
+            System.out.println("View button clicked for user: " + NameLabel.getText());
+            if (parentController != null) {
+                parentController.viewUserDetails(user);
+            }
+        });
     }    
-    
-    
+
+    private void loadAccountsFromDatabase() {
+        try (Connection connection = DriverManager.getConnection(Constant.DB_URL, Constant.DB_USERNAME, Constant.DB_PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT usersid, username, mobilenum FROM users_information")) {
+
+            while (resultSet.next()) {
+                int usersid = resultSet.getInt("usersid");
+                String name = resultSet.getString("username");
+                int mobilenum = resultSet.getInt("mobilenum");
+                userInfoName.put(usersid, name + "\t");
+                userInfoNum.put(usersid, mobilenum);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void updateItem(UserAvatar user, boolean empty) {
+        super.updateItem(user, empty);
+        if (empty || user == null) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            if (loader == null) {
+                loader = new FXMLLoader(getClass().getResource("/FXMLFiles/ViewUserCell.fxml"));
+                loader.setController(this);
+
+                try {
+                    loader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            HBox hBox = new HBox(10);
+            String username = userInfoName.get(user.getUserId());
+            NameLabel.setText(username != null ? username.toLowerCase() : "");
+            NoAccLabel.setText("| " + userInfoName);
+            String imagePath = "file:" + user.getImagePath();
+            ImageView imageView = new ImageView();
+            try {
+                Image image = new Image(imagePath);
+                imageView.setImage(image);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Error loading image: " + e.getMessage());
+            }
+            imageView.setFitHeight(40);
+            imageView.setFitWidth(40);
+            Circle circle = new Circle(18, 18, 18);
+            imageView.setClip(circle);
+            setGraphic(Base);
+            DeleteBtn.setOnAction(event -> {
+                System.out.println("Delete button clicked for user: " + NoAccLabel.getText());
+                if (parentController != null) {
+                    parentController.deleteUser(user);
+                }
+            });
+            NameLabel = new Label(userInfoName.get(user.getUserId()));
+            NoAccLabel = new Label(userInfoNum.get(user.getUserId()) + "");
+            hBox.getChildren().addAll(imageView, NameLabel, NoAccLabel, ViewBtn, DeleteBtn);
+            setGraphic(hBox);
+
+        }
+    }
     public void setUserInformationDisplay(String imagePath, String username, String accountNumber){
         File file = new File(imagePath);
         UserPfp.setImage(new Image(file.toURI().toString()));
